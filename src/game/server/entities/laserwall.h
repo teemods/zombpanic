a65a26579c5d8a3d1c768b92b6ab6f7ed2e6aae5
srdante <minidantebtc@gmail.com>
2022-03-26 02:18:11 -0300

    Fix header guard

--- src/game/server/entities/laserwall.h
+++ src/game/server/entities/laserwall.h
@@ -1,7 +1,7 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#ifndef GAME_SERVER_ENTITIES_LASER_WALL_H
-#define GAME_SERVER_ENTITIES_LASER_WALL_H
+#ifndef GAME_SERVER_ENTITIES_LASERWALL_H
+#define GAME_SERVER_ENTITIES_LASERWALL_H
 
 #include <game/server/entity.h>
 
